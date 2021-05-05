fe0320b1e6f6d01f8d3dfffbcdf2a1e9fe4705c8
Hyeahn Lee <lha@Hyeahns-MacBook-Pro.local>
2021-05-05 19:30:07 +0900

    프로젝트명 변경

---
+++ client/pages/_app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const App = ({ Component }) => {
+  return (
+    <Component />
+  );
+}
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired
+};
+
+export default App;
