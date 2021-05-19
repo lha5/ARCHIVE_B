deb2b662a82d50cae2f34e173ed4f8ae40b53182
Hyeahn Lee <lha@Hyeahns-MacBook-Pro.local>
2021-05-19 20:07:59 +0900

    _app.js 수정

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Head from 'next/head';
+import 'semantic-ui-css/semantic.min.css';
 
 const App = ({ Component }) => {
   return (
-    <Component />
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>AB</title>
+      </Head>
+      <Component />
+    </>
   );
-}
+};
 
 App.propTypes = {
-  Component: PropTypes.elementType.isRequired
+  Component: PropTypes.elementType.isRequired,
 };
 
 export default App;
